4e74102c8fd7ec3ceadf6d75279aca10ff232231
hitbotK <121420138+hitbotK@users.noreply.github.com>
2025-05-01 14:47:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import yt_dlp
 import os
 import uuid
 
-app = Flask(__name__, template_folder='templates') 
+app = Flask(__name__, template_folder='templates')  # ชี้ไปยังโฟลเดอร์ templates
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
